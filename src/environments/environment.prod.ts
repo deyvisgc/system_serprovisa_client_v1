df01207184967182bf5d49699a7aed76adf90de5
deyvisgc <deivisgarcia@upeu.edu.pe>
2024-02-09 16:52:34 -0500

    demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //API_BASE_URL: 'https://apicomercial.serprovisa.com.pe/api/v1'
-  API_BASE_URL: 'https://apialmacen.serprovisa.com.pe/api/v1'
+  //API_BASE_URL: 'https://apialmacen.serprovisa.com.pe/api/v1'
+  API_BASE_URL: 'https://apidemo.serprovisa.com.pe/api/v1'
 };
